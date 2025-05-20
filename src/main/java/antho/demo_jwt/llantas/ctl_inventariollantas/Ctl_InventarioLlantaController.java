668381afdcdf8837b0e1424cfc56339232c16b13
anthocoppelito <practicante.dps@coppel.com>
2025-05-20 09:13:45 -0700

    Eliminar importación no utilizada de UserDTO en Ctl_InventarioLlantaController

--- src/main/java/antho/demo_jwt/llantas/ctl_inventariollantas/Ctl_InventarioLlantaController.java
+++ src/main/java/antho/demo_jwt/llantas/ctl_inventariollantas/Ctl_InventarioLlantaController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import antho.demo_jwt.User.UserDTO;
 import lombok.RequiredArgsConstructor;
 
 @RestController
